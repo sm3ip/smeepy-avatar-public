df7a90a000b6574292f8c85ef23d375470bbf263
sm3ip <lucasomenpc@gmail.com>
2021-11-18 11:54:56 +0100

    new login method
    new way of login uses a file which contains three lines :
    line 1 : the username
    line 2 : the channel name
    line 3 : the Twitch Chat OAuth Password

--- Assets/scripts/log_button.cs
+++ Assets/scripts/log_button.cs
@@ -7,7 +7,8 @@ public class log_button : MonoBehaviour
 {
     public GameObject menu; // get the menu
     public bool on_screen= true; // check if the menu is visible
-    public GameObject channel_input, username_input, oauth_input, t_chat; // get the input fields and the twitch chat object
+    public GameObject path_input, t_chat; // get the input field and the twitch chat object
+    public string[] file; // create an array of string to store the file
     private TwitchChat script_t_chat; // links twitch chat script
  
     void Start(){
@@ -22,7 +23,21 @@ public class log_button : MonoBehaviour
 
     public void loginStart()
     {
+        /*
+        the file used to connect is composed of three lines :
+        line 1 : username
+        line 2 : channel name
+        line 3 : Twitch Chat OAuth Password
+        */
         // called by clicking the start menu : get the input fields values and modify them in the twitch chat script
-        script_t_chat.mod_co_values(username_input.GetComponent<TMP_InputField>().text,channel_input.GetComponent<TMP_InputField>().text,oauth_input.GetComponent<TMP_InputField>().text);
+        file=readFile1(path_input.GetComponent<TMP_InputField>().text); // reads the file and stores it in an array of strings
+        script_t_chat.mod_co_values(file[0], file[1], file[2]); // call the void to modify the login values
+    }
+
+    public string[] readFile1( string path)
+    {
+        // reads a file and stores it's values
+        string[] lines = System.IO.File.ReadAllLines(path);
+        return lines;
     }
 }
