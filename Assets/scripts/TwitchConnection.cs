735d7f0785a6c4c87eb4b8fefb25292c39195bc0
sm3ip <lucasomenpc@gmail.com>
2021-12-31 14:51:16 +0100

    twitch methods clean
    all methods are correctly written just needs to link them to the already existing system + some cleaning to do

--- Assets/scripts/TwitchConnection.cs
+++ Assets/scripts/TwitchConnection.cs
@@ -24,7 +24,6 @@ public class TwitchConnection : ScriptableObject
 
     public async void ConnectToTwitch()
     {
-        Debug.Log("Connecting.......");
         _tcpClient = new TcpClient();
         await _tcpClient.ConnectAsync("irc.chat.twitch.tv", 6667);
         _streamReader = new StreamReader(_tcpClient.GetStream());
@@ -33,16 +32,90 @@ public class TwitchConnection : ScriptableObject
         await _streamWriter.WriteLineAsync("NICK " + accountName);
 
         ReadMessages();
+        JoinChan(accountName);
     }
 
-    public string lastLine;
+    public async void WriteInChan(string message, string chanName)
+    {
+        await _streamWriter.WriteLineAsync($"PRIVMSG #{chanName} :{message}");
+    }
+
+    public async void JoinChan(string chanName)
+    {
+        await _streamWriter.WriteLineAsync("JOIN #" + chanName);
+    }
+
+    public async void LeaveChan(string chanName)
+    {
+        await _streamWriter.WriteLineAsync("PART #" + chanName);
+    }
+
+    private string lastLine;
+    [SerializeField] private List<string> logs = new List<string>();
+    [SerializeField] private int logsIndex;
+    [SerializeField] private bool isClearLogs = false;
 
     private async void ReadMessages()
     {
+        logs.Clear();
+        logsIndex = 1;
         while (true)
         {
+            if (isClearLogs)
+            {
+                logs.Clear();
+                logsIndex = 1;
+                isClearLogs = false;
+                logs.Add(lastLine);
+            }
             lastLine = await _streamReader.ReadLineAsync();
+            logs.Add(lastLine);
             Debug.Log(lastLine);
+            
+            if (lastLine != null && lastLine.StartsWith("PING"))
+            {
+                lastLine.Replace("PING", "PONG");
+                await _streamWriter.WriteLineAsync(lastLine);
+            }
         }
+    }
+
+    public void ClearLogs()
+    {
+        isClearLogs = true;
+    }
+
+    public bool NewTwitchMessage(out string newMessage) // call this one to check if there is another message to take care of and use the out in the cut message right below
+    {
+        if (logs.Count < logsIndex)
+        {
+            newMessage = "";
+            return false;
+        }
+
+        for (int i = logsIndex; i <= logs.Count; i++)
+        {
+            if (logs[i-1].Contains("PRIVMSG"))
+            {
+                logsIndex = i + 1;
+                newMessage = logs[i - 1];
+                return true;
+            }
+        }
+
+        logsIndex = logs.Count + 1;
+        newMessage = "";
+        return false;
+    }
+
+    public string[] CutMessage(string rawMessage)
+    {
+        if (rawMessage== null || !rawMessage.Contains("PRIVMSG"))
+        {
+            return new string[1] {""};
+        }
+        string message = rawMessage.Split(':')[2];
+        string user = rawMessage.Split(' ')[0].Split('!')[0].Substring(1);
+        return new string[2] {user, message};
     }
 }
